956c9682fde75900516934d1aeda3ceddcb64551
guarantee418 <66642037+guarantee418@users.noreply.github.com>
2025-07-14 23:41:23 -0400

    Update hrf.scala

--- haunt-roll-fail/hrf.scala
+++ haunt-roll-fail/hrf.scala
@@ -103,7 +103,11 @@ object HRF {
         yarg.Meta -> yarg.UI,
     )
 
-    val metas = metaUIs.lefts.%(_.path != "root" || HRF.lobby.any || offline)
+   val metas = metaUIs.lefts.%(_.path != "root" || HRF.lobby.any || offline)
+
+   HRF.param("meta").or("root")
+  ./~(mn => HRF.metas.%(_.name == mn).single)
+  ./{ m => new HRFMetaUI(this, m, 800).withMeta() }
 
     val html = dom.window.location.origin + "/play/"
     val script = dom.document.getElementById("script").asInstanceOf[dom.html.Script].src
